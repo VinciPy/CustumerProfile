d6a50c62d85aea80a6bdd2eb1b2a3c4bd06b18a6
Vinicius Santana <viniciusantana323@gmail.com>
2022-01-10 23:03:30 -0300

    feat: implement entity client

--- src/domain/Entities/Client.ts
+++ src/domain/Entities/Client.ts
@@ -1,5 +1,53 @@
+import Devices from "./Devices";
+import Location from "./Location";
+import Visit from "./Visit";
+
 export default class Cliente {
-    private UUID;
-    private Location;
-    private Devices;
+    private _UUID: string;
+    private Location: Location;
+    private Devices: Devices[];
+    private Visits: Visit[];
+    private FirstVisit: Visit;
+
+    public getFirstVisit(): Visit {
+        return this.FirstVisit;
+    }
+    public setFirstVisit(value: Visit) {
+        this.FirstVisit = value;
+    }
+
+    public getUUID(): string {
+        return this._UUID;
+    }
+
+    public setUUID(value: string) {
+        this._UUID = value;
+    }
+
+    public getLocation(): Location {
+        return this.Location;
+    }
+
+    public setLocation(value: Location) {
+        this.Location = value;
+    }
+    public getDevices(): Devices[] {
+        return this.Devices;
+    }
+
+    public setDevices(value: Devices[]) {
+        this.Devices = value;
+    }
+
+    public getVisits(): Visit[] {
+        return this.Visits;
+    }
+    public setVisits(value: Visit[]) {
+        this.Visits = value;
+    }
+
+    constructor() {
+        
+    };
+
 }
